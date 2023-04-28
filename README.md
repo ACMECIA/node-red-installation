8cbd9929cda5a414bc55d68d7b2a0e25a5421360
Del Piero Flores <delpier22.flores@gmail.com>
2023-04-28 16:42:07 -0500

    u

---
+++ README.md
@@ -0,0 +1,6 @@
+Installation of NodeRED
+=======================
+
+1. ensure, that setup.py is executable: 'chmod u+x setup.py'
+2. call './setup.py'
+3. reboot to complete installation
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf8
+
+import os
+import sys
+from datetime import date
+
+
+def main():
+	print("Installing Node-RED...")
+	print("1. Ensure that the device has a connection to the Internet\n2. Check if the date is correct (current date: " + str(date.today()) + ").")
+	print("The official install script will be started now. Unless you have any other requirements, we recommend to answer all following questions with y (yes).")
+	res = raw_input("Would you like to proceed? [y/N] ")
+	if not (res == "y" or res == "Y"):
+		print("Abort.")
+		sys.exit(1)
+
+	os.system("bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)")
+	os.system("systemctl enable nodered")
+	os.system("systemctl start nodered")
+	os.system("sync")
+
+if __name__ == '__main__':
+	main()
